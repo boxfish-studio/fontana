3b4e52327b65c21e160f82c5ed8d4b981fab94e6
Arnau Espin <arnauespin@gmail.com>
2022-06-14 09:31:13 +0200

    Styled MintRow

--- components/MintRow.tsx
+++ components/MintRow.tsx
@@ -1,50 +1,84 @@
-import { useConnection } from "@solana/wallet-adapter-react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { rpcMethods } from "lib/spl";
 import { useEffect, useState } from "react";
+import styles from "styles/MintRow.module.css";
 
 const owner = "BoX451MZzydoVdZE4NFfmMT3J5Ztqo7YgUNbwwMfjPFu";
 
 const MintRow: React.FC<{
   token: string;
-}> = ({ token }) => {
-  const [amount, setAmount] = useState(10);
+  badge: string;
+}> = ({ token, badge }) => {
   const { connection } = useConnection();
+  const { connected } = useWallet();
+  const [mintedAmount, setMintedAmount] = useState<null | number>(null);
+  const [walletAmount, setWalletAmount] = useState<null | number>(null);
   const [isMinting, setIsMinting] = useState(false);
+  const [mintAmount, setMintAmount] = useState(0);
+  const [transferAmount, setTransferAmount] = useState(0);
+
   async function getTokenBalance() {
     const amount = await new rpcMethods(connection).getTokenBalance(
       owner,
       token
     );
-
-    setAmount(amount);
+    setMintedAmount(amount);
+    if (!connected) return;
+    const walletAmount = await new rpcMethods(connection).getTokenBalance(
+      owner,
+      token
+    );
+    setWalletAmount(walletAmount);
   }
 
   useEffect(() => {
     getTokenBalance();
-  }, []);
+  }, [connected]);
 
   async function mintTokens() {
-      setIsMinting(true)
+    setIsMinting(true);
+    if (mintAmount === 0) {
+      setIsMinting(false);
+      return;
+    }
     const res = await fetch("api/mint/", {
       method: "POST",
       body: JSON.stringify({
         owner,
         token,
-        amount: 4,
+        amount: mintAmount,
       }),
     });
     console.log("res", await res.json());
-    setIsMinting(false)
+    setIsMinting(false);
 
     getTokenBalance();
   }
 
   return (
-    <div>
-      {token}-{amount}
-      {isMinting && <div>Minting...</div>}
-      <button onClick={mintTokens}>Mint more</button>
-      <button>Transfer</button>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <span> Badge: {badge}</span>
+        <span> Mint address: {token}</span>
+        <span> Minted amount: {mintedAmount}</span>
+        <span> My amount: {walletAmount}</span>
+      </div>
+      <span className={styles.isMinting}>
+        {isMinting && <div>Minting...</div>}
+      </span>
+      <div className={styles.buttons}>
+        <input
+          value={mintAmount}
+          onChange={(e) => setMintAmount(parseInt(e.target.value))}
+        />
+        <button onClick={mintTokens}>Mint more</button>
+        <input
+          value={transferAmount}
+          onChange={(e) => setTransferAmount(parseInt(e.target.value))}
+        />
+
+        <button>Transfer</button>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,8 @@ const Home: NextPage<Props> = ({ SPL_TOKENS }) => {
       </Head>
 
       <main className={styles.main}>
-        {SPL_TOKENS.map((token) => (
-          <MintRow key={token} token={token} />
+        {SPL_TOKENS.map((token,i) => (
+          <MintRow key={token} token={token} badge={BADGES[i]}/>
         ))}
       </main>
     </div>
