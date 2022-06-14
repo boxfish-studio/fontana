ab4b0b7abad808567eb97b2e3e3504c2d666a5c9
Arnau Espin <arnauespin@gmail.com>
2022-06-14 09:47:32 +0200

    Transfer APi call

--- components/MintRow.tsx
+++ components/MintRow.tsx
@@ -10,10 +10,10 @@ const MintRow: React.FC<{
   badge: string;
 }> = ({ token, badge }) => {
   const { connection } = useConnection();
-  const { connected } = useWallet();
+  const { connected, publicKey } = useWallet();
   const [mintedAmount, setMintedAmount] = useState<null | number>(null);
   const [walletAmount, setWalletAmount] = useState<null | number>(null);
-  const [isMinting, setIsMinting] = useState(false);
+  const [action, setAction] = useState<null | string>(null);
   const [mintAmount, setMintAmount] = useState(0);
   const [transferAmount, setTransferAmount] = useState(0);
 
@@ -23,9 +23,9 @@ const MintRow: React.FC<{
       token
     );
     setMintedAmount(amount);
-    if (!connected) return;
+    if (!publicKey) return;
     const walletAmount = await new rpcMethods(connection).getTokenBalance(
-      owner,
+      publicKey.toBase58(),
       token
     );
     setWalletAmount(walletAmount);
@@ -36,9 +36,9 @@ const MintRow: React.FC<{
   }, [connected]);
 
   async function mintTokens() {
-    setIsMinting(true);
+    setAction("Minting...");
     if (mintAmount === 0) {
-      setIsMinting(false);
+      setAction(null);
       return;
     }
     const res = await fetch("api/mint/", {
@@ -50,7 +50,28 @@ const MintRow: React.FC<{
       }),
     });
     console.log("res", await res.json());
-    setIsMinting(false);
+    setAction(null);
+
+    getTokenBalance();
+  }
+
+  async function transferTokens() {
+    setAction("Transferring...");
+    if (transferAmount === 0 || !publicKey) {
+      setAction(null);
+      return;
+    }
+    const res = await fetch("api/transfer/", {
+      method: "POST",
+      body: JSON.stringify({
+        owner,
+        token,
+        amount: transferAmount,
+        recipient: publicKey.toBase58(),
+      }),
+    });
+    console.log("res", await res.json());
+    setAction(null);
 
     getTokenBalance();
   }
@@ -63,9 +84,7 @@ const MintRow: React.FC<{
         <span> Minted amount: {mintedAmount}</span>
         <span> My amount: {walletAmount}</span>
       </div>
-      <span className={styles.isMinting}>
-        {isMinting && <div>Minting...</div>}
-      </span>
+      <span className={styles.isMinting}>{action && <div>{action}</div>}</span>
       <div className={styles.buttons}>
         <input
           value={mintAmount}
@@ -77,7 +96,7 @@ const MintRow: React.FC<{
           onChange={(e) => setTransferAmount(parseInt(e.target.value))}
         />
 
-        <button>Transfer</button>
+        <button onClick={transferTokens}>Transfer</button>
       </div>
     </div>
   );
