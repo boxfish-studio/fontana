876eb27e5abed3fe3405f601a84b67a016831421
Arnau Espin <arnauespin@gmail.com>
2022-07-28 13:16:16 +0200

    fix

--- components/Layout/index.ts
+++ components/Layout/index.ts
@@ -3,4 +3,3 @@ export { default as Header } from "./Header";
 export { default as FontanaSVG } from "./FontanaSVG";
 export { default as NetworkSelector } from "./NetworkSelector";
 export { default as Toast } from "./Flash";
-export { default as NetworkSelector } from "./NetworkSelector";
