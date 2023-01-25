8dabce8d8e996084095f13aa321bc853a7601981
Arnau Espin <arnauespin@gmail.com>
2023-01-25 10:10:54 +0100

    feat: add custom wallet

--- components/Layout/Wallet.tsx
+++ components/Layout/Wallet.tsx
@@ -1,15 +1,25 @@
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from 'components/WalletWrapper';
 import { FC } from 'react';
 
-const Wallet: FC = () => (
-  <div
-    className="d-flex "
-    style={{
-      transform: 'scale(0.7)',
-    }}
-  >
-    <WalletMultiButton />
-  </div>
-);
+const Wallet: FC = () => {
+  const { publicKey } = useWallet();
+  return (
+    <div
+      className="d-flex "
+      style={{
+        transform: 'scale(0.7)',
+      }}
+    >
+      <WalletMultiButton className="solana-wallet-adapter-button">
+        {!publicKey
+          ? 'Connect Wallet'
+          : publicKey?.toBase58().slice(0, 4) +
+            '..' +
+            publicKey?.toBase58().slice(-4)}{' '}
+      </WalletMultiButton>
+    </div>
+  );
+};
 
 export default Wallet;
